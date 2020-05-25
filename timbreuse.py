727dcf93cd1619dd6b7220c0334626aacc74289c
josmet52 <joseph.metrailler@bluewin.ch>
2020-05-25 23:18:03 +0200

    Ça avance

--- timbreuse.py
+++ timbreuse.py
@@ -19,6 +19,10 @@ import time
 import pdb
 import datetime
 import signal
+import pdb
+
+from mysql_lib_timbreuse import MysqlTimbreuse
+
 
 class Timbreuse:
     """
@@ -27,6 +31,25 @@ class Timbreuse:
     def __init__(self):
         
         self.button_working = False
+        self.root_working = False
+        self.ip_db_server = "192.168.1.142"
+        self.mysql_timbreuse = MysqlTimbreuse(self.ip_db_server)
+        self.choose_person = False      
+        self.choose_task = False
+        
+        self.qui_en_cours = ""
+        self.no_qui_en_cours = 0
+        self.id_qui_en_cours = 0
+        
+        self.quoi_en_cours = ""
+        self.no_quoi_en_cours = 0
+        self.id_quoi_en_cours = 0
+        
+        self.tasks_running = []
+        
+        self.id_qui_stop_en_cours = 0
+        self.qui_stop_en_cours = ""
+        self.stop_running = False
         
         # Define GPIO to LCD mapping
         self.LCD_RS = 7
@@ -92,6 +115,11 @@ class Timbreuse:
 
     def button_pressed_callback(self, channel):
 
+# tbd_01 begin --> a améliorer
+#         if self.root_working : # si le root est en train d'actualiser l'affichage : ne rien faire
+#             return # si le root est en train d'actualiser l'affichage : ne rien faire
+# tbd_01 end        
+
         self.button_working = True
         
         # red button
@@ -129,29 +157,226 @@ class Timbreuse:
         else:
             self.gray_btn_fire = False
             
+#         self.button_working = False
+
+    def red_button_fired(self): # on quitte le boulot
+
+        # are persons working ? mo -> du nothing
+        if len(self.tasks_running) == 0:
+            return
+        # create the list of working persons
+        self.id_qui_stoping = self.tasks_running[0][0]
+        self.qui_stoping = self.tasks_running[0][3]
+        self.no_qui_stoping = 0
+        self.stop_running = True
+        
+        self.lcd_string(self.qui_stoping, self.LCD_LINE_1)
+        self.lcd_string("stop job ?", self.LCD_LINE_2)
+        
+
+    def green_button_fired(self): # on commence le boulot
+        
+        # first step
+            # display the first person
+            # flag one_person_starting_the_job
+            
+        if not self.choose_person and not self.choose_task:
+            # connect to the db
+            con, e = self.mysql_timbreuse.get_db_connexion()
+            cur = con.cursor()
+            # select the first person
+            sql_txt = "SELECT prenom, idqui FROM qui ORDER BY prenom LIMIT 1;" 
+            cur.execute(sql_txt)
+            rec = cur.fetchone()
+            self.qui_en_cours = rec[0]
+            self.no_qui_en_cours = 0
+            self.id_qui_en_cours = rec[1]
+            # update display
+            self.lcd_string(self.qui_en_cours, self.LCD_LINE_1)
+            self.lcd_string("", self.LCD_LINE_2)
+            self.choose_person = True
+            print("self.qui_en_cours:", self.qui_en_cours)
+            # close the connexion
+            cur.close()
+            con.close()
+
+    def blue_button_fired(self): # down
+        
+        # if one_person_starting_the_job : display the next person
+        # if one_person_choosing_the_task : display the next task
+        
+        # connect to the db
+        con, e = self.mysql_timbreuse.get_db_connexion()
+        cur = con.cursor()
+        # find the next person
+        if self.choose_person:
+            # find the last idqui
+            sql_txt = "SELECT prenom, idqui FROM qui ORDER BY prenom;"
+            cur.execute(sql_txt)
+            rec = cur.fetchall()
+            nbre_qui = len(rec) - 1
+            # find the next person 
+            if self.no_qui_en_cours >= nbre_qui:
+                # if last then restart to first
+                self.no_qui_en_cours = 0
+            else:
+                # else select the next
+                self.no_qui_en_cours += 1
+            # execute the query
+            self.qui_en_cours = rec[self.no_qui_en_cours][0]
+            self.id_qui_en_cours = rec[self.no_qui_en_cours][1]
+            # display the person
+            self.lcd_string(self.qui_en_cours, self.LCD_LINE_1)
+            print("self.qui_en_cours:", self.qui_en_cours)
+            
+        elif self.choose_task:
+            # find the last idquoi
+            sql_txt = "".join(["SELECT quoi.activite, quoi.idquoi",
+                               " FROM qui INNER JOIN (quoi INNER JOIN quiquoi ON (quoi.idquoi = quiquoi.idquoi)",
+                               " AND (quoi.idquoi = quiquoi.idquoi)) ON (qui.idqui = quiquoi.idqui) AND (qui.idqui = quiquoi.idqui)",
+                               " WHERE (((qui.prenom)='" , str(self.qui_en_cours), "'))",
+                               " ORDER BY quoi.activite;"])
+            cur.execute(sql_txt)
+            rec = cur.fetchall()
+            nbre_quoi = len(rec) - 1
+            # find the next person 
+            if self.no_quoi_en_cours >= nbre_quoi:
+                # if last then restart to first
+                self.no_quoi_en_cours = 0
+            else:
+                # else select the next
+                self.no_quoi_en_cours += 1
+            print("self.no_quoi_en_cours:", self.no_quoi_en_cours, "nbre_quoi:", nbre_quoi)
+                # if last then restart to first
+            self.quoi_en_cours = rec[self.no_quoi_en_cours][0]
+            self.id_quoi_en_cours = rec[self.no_quoi_en_cours][1]
+            # display the person
+            self.lcd_string(self.quoi_en_cours, self.LCD_LINE_2)
+            print("self.qui_en_cours:", self.qui_en_cours, "self.quoi_en_cours:", self.quoi_en_cours)
+            
+        elif self.stop_running:
+            
+            nbre_qui_running = len(self.tasks_running)
+            if nbre_qui_running <= 1:
+                return # do nothing because there is only on peron working
+                
+            # find the next person 
+            if self.no_qui_stoping >= nbre_qui_running - 1:
+                # if last then restart to first
+                self.no_qui_stoping = 0
+            else:
+                # else select the next
+                self.no_qui_stoping += 1
+            print(self.no_qui_stoping)
+            # execute the query
+            self.qui_stoping = self.tasks_running[self.no_qui_stoping][3]
+            self.id_qui_stoping = self.tasks_running[self.no_qui_stoping][0]
+            # display the person
+        
+            self.lcd_string(self.qui_stoping, self.LCD_LINE_1)
+            self.lcd_string("stop job ?", self.LCD_LINE_2)
+            
+       # close the connexion
+        cur.close()
+        con.close()
+        
+
+    def yellow_button_fired(self): # ok
+                
+                
+        
+        # connect to the db
+        con, e = self.mysql_timbreuse.get_db_connexion()
+        cur = con.cursor()
+        # close choose_person and start choose_task
+        if self.choose_person:
+        
+            for task in self.tasks_running:
+    #             pdb.set_trace()
+                if task[0] == self.id_qui_en_cours:
+                    disp_txt_1 = " ".join([task[3][0], "->", task[4]])
+                    disp_txt_2 = " ".join(["stop before new"])
+                    
+                    self.lcd_string(disp_txt_1, self.LCD_LINE_1)
+                    self.lcd_string(disp_txt_2, self.LCD_LINE_2)
+                    
+                    self.choose_person = False
+                    self.choose_task = False
+                    return
+                
+            # find the tasks for this user first the default ans after order by activite
+            sql_txt = "".join(["SELECT quoi.activite",
+                               " FROM qui INNER JOIN (quoi INNER JOIN quiquoi ON (quoi.idquoi = quiquoi.idquoi)",
+                               " AND (quoi.idquoi = quiquoi.idquoi)) ON (qui.idqui = quiquoi.idqui) AND (qui.idqui = quiquoi.idqui)",
+                               " WHERE (((qui.prenom)='" , str(self.qui_en_cours), "'))",
+                               " ORDER BY quoi.activite;"])
+#             print(sql_txt)
+            cur.execute(sql_txt)
+            rec = cur.fetchone()
+            self.no_quoi_en_cours = 0
+            self.quoi_en_cours = rec[0]
+            # display the first task
+            self.lcd_string(self.quoi_en_cours, self.LCD_LINE_2)
+            print("self.qui_en_cours:", self.qui_en_cours, "self.quoi_en_cours:", self.quoi_en_cours)
+            
+            self.choose_person = False
+            self.choose_task = True
+            
+        elif self.choose_task:
+            
+            self.choose_person = False
+            self.choose_task = False
+            disp_txt_1 = " ".join([str(self.qui_en_cours)[0], "->", str(self.quoi_en_cours)])
+            disp_txt_2 = " ".join(["Starting", time.strftime("%H:%M")])
+            self.lcd_string(disp_txt_1, self.LCD_LINE_1)
+            self.lcd_string(disp_txt_2, self.LCD_LINE_2)
+            
+            print("qui", self.qui_en_cours, "id", self.id_qui_en_cours)
+            print("quoi", self.quoi_en_cours, "id", self.id_quoi_en_cours)
+            
+            sql_txt = "INSERT INTO quand (idqui, idquoi, begindatetime) VALUES(%s, %s, %s)"
+            val_tuple = (str(self.id_qui_en_cours), str(self.id_quoi_en_cours), time.strftime("%Y-%m-%d %H:%M:%S"))
+            cur.execute(sql_txt, val_tuple)
+            con.commit()
+            print(sql_txt, val_tuple)
+            
+            self.tasks_running.append((self.id_qui_en_cours, self.id_quoi_en_cours, time.strftime("%Y-%m-%d %H:%M:%S"), self.qui_en_cours, self.quoi_en_cours))
+
+        elif self.stop_running:
+            id_qui_stoping = self.tasks_running[self.no_qui_stoping][0]
+            id_quoi_stoping = self.tasks_running[self.no_qui_stoping][1]
+            
+            sql_txt = "".join(["UPDATE quand SET enddatetime='", time.strftime("%Y-%m-%d %H:%M:%S"), "'"
+                               " WHERE (idqui=", str(id_qui_stoping), " AND idquoi=", str(id_quoi_stoping)," AND enddatetime IS NULL);"])
+            cur.execute(sql_txt)
+            con.commit()
+            print(sql_txt)
+
+            disp_txt_1 = " ".join([str(self.tasks_running[self.no_qui_stoping][3])[0], "->", str(self.tasks_running[self.no_qui_stoping][4])])
+            disp_txt_2 = " ".join(["Stoping", time.strftime("%H:%M")])
+            self.lcd_string(disp_txt_1, self.LCD_LINE_1)
+            self.lcd_string(disp_txt_2, self.LCD_LINE_2)
+
+
+
+#         cur.close()
+        con.close()
+#         self.button_working = False
+
+    def gray_button_fired(self): # cancel
+
+        # cancel the running operation
+        # reset all the flags
+        # dispaly date and time
+        self.choose_person = False
+        self.choose_task = False
+        self.qui_en_cours = '0'
+        self.idquoi_en_cours = '0'
+        self.lcd_string(str(datetime.datetime.now()),self.LCD_LINE_1)
+        self.lcd_string("                ", self.LCD_LINE_2)
         self.button_working = False
-
-    def red_button_fired(self):
-        self.lcd_string(str(datetime.datetime.now()),self.LCD_LINE_1)
-        self.lcd_string("red fired", self.LCD_LINE_2)
-
-    def green_button_fired(self):
-        self.lcd_string(str(datetime.datetime.now()),self.LCD_LINE_1)
-        self.lcd_string("green fired", self.LCD_LINE_2)
-
-    def blue_button_fired(self):
-        self.lcd_string(str(datetime.datetime.now()),self.LCD_LINE_1)
-        self.lcd_string("blue fired", self.LCD_LINE_2)
-
-    def yellow_button_fired(self):
-        self.lcd_string(str(datetime.datetime.now()),self.LCD_LINE_1)
-        self.lcd_string("yellow fired", self.LCD_LINE_2)
-
-    def gray_button_fired(self):
-        self.lcd_string(str(datetime.datetime.now()),self.LCD_LINE_1)
-        self.lcd_string("gray fired", self.LCD_LINE_2)
  
-    def lcd_init(self):
+    def lcd_init(self): 
         # Initialise display
         self.lcd_byte(0x33, self.LCD_CMD) # 110011 Initialise
         self.lcd_byte(0x32, self.LCD_CMD) # 110010 Initialise
@@ -239,10 +464,12 @@ class Timbreuse:
             if elapsed >= pause_time:
                 while self.button_working:
                     time.sleep(0.1)
+                self.root_working = True
                 self.lcd_string(str(datetime.datetime.now()),self.LCD_LINE_1)
                 self.lcd_string("                ", self.LCD_LINE_2)
                 last_time = datetime.datetime.now()
                 time.sleep(pause_time -0.5)
+                self.root_working = False
             
         
 if __name__ == '__main__':
